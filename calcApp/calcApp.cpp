7a226c4f58ff239412fc712a82271004e7ccf5ed
StShu <32334744+StShu@users.noreply.github.com>
2020-10-16 12:00:15 +0700

    Update calcApp.cpp

--- calcApp/calcApp.cpp
+++ calcApp/calcApp.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "RUSSIAN");
+
     float x, y;
     cin >> x >> y;
-    cout << sum(x, y);
+    cout << sum(x, y) << endl;
+    cout << divs(x, y) << endl;
+    cout << multiP(x, y) << endl;
+    cout << diff(x, y) << endl;
 }
