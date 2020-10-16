b2993857ed698f8ccbb46f7a332551aa976eb26d
StShu <32334744+StShu@users.noreply.github.com>
2020-10-16 12:00:12 +0700

    Update functions.cpp

--- calcApp/functions.cpp
+++ calcApp/functions.cpp
@@ -4,3 +4,21 @@ float sum(float x, float y)
 {
     return x + y;
 }
+
+float diff(float x, float y)
+{
+    return x - y;
+}
+
+float multiP(float x, float y)
+{
+    return x * y;
+}
+
+float divs(float x, float y)
+{
+    if (y == 0) {
+        cout << "Делить на ноль нельзя!" << endl;
+    } else
+        return x / y;
+}
