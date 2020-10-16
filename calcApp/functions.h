0518e3fc37c59bf000ea44554f420836117a51c6
StShu <32334744+StShu@users.noreply.github.com>
2020-10-16 11:59:52 +0700

    Update functions.h
    Объявил  новыек функции

--- calcApp/functions.h
+++ calcApp/functions.h
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 using namespace std;
 
 float sum(float x, float y);
+
+float diff(float x, float y);
+
+float multiP(float x, float y);
+
+float divs(float x, float y);
