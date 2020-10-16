9ad5c21a1c807b0cc5e8c15d6f32e45825b9250d
StShu <32334744+StShu@users.noreply.github.com>
2020-10-16 13:42:20 +0700

    Update test.cpp
    Тесты основных функций

--- test/test.cpp
+++ test/test.cpp
@@ -2,8 +2,25 @@
 #define CATCH_CONFIG_MAIN
 #include "../catch/catch.hpp"
 
-TEST_CASE("test1")
+TEST_CASE("Test Amount")
 {
-    float expected_bull = 8;
-    REQUIRE(expected_bull == sum(2, 4));
+    float expected = 6;
+    REQUIRE(expected == sum(2, 4));
+}
+TEST_CASE("Test Difference")
+{
+    float expected = 7.5;
+    REQUIRE(expected == diff(10, 2.5));
+}
+
+TEST_CASE("Test Multiplication")
+{
+    float expected = 6;
+    REQUIRE(expected == multiP(2, 4));
+}
+
+TEST_CASE("Test Division")
+{
+    float expected = 5;
+    REQUIRE(expected == divs(10, 2));
 }
