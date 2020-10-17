a744ccb67b19ef301a0bb6d41c6b1da9ae18e73a
StShu <32334744+StShu@users.noreply.github.com>
2020-10-17 20:03:43 +0700

    Update calcApp.cpp
    Убрал недочеты по движению в меню

--- calcApp/calcApp.cpp
+++ calcApp/calcApp.cpp
@@ -6,33 +6,31 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "RUSSIAN");
-    int oper;
-    cout << "1 - Обычные операции\n2 - Тригонометрические функции 0 - Выход " << endl;
-    cout <<"Введите номер : ";
-    cin >> oper;
+    char oper;
+    
     while (true) {
+        cout << "1 - Обычные функций\n2 - Тригонометрические функции 0 - " "Выход " << endl;
+        cout << "Введите номер : ";
+        cin >> oper;
         switch (oper) {
-        case 1:
+        case '1': 
             system("cls");
             menu_math();
             break;
-        case 2:
+        case '2':
             system("cls");
             break;
-        case 0:
+        case '0':
             return 0;
             break;
         default:
             cout << "Не верный выбор !" << endl;
             break;
         }
-
         system("cls");
-        cout << "Повторить? y/n" << endl;
+        cout << "Повторить выбор функций? y/n" << endl;
         cin >> oper;
-
         if (oper == 'n')
-            break;
+            return 0;
     }
-    return 0;
 }
